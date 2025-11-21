74c0259984c1c3e704e3a1eb435711068294f618
ram <ramlakhanregar1+github@gmial.com>
2025-11-21 21:46:50 +0530

    fix

--- external-folder/backend/src/db/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connect = () => {
-    mongoose.connect(process.env.MONGO_URL) 
-        .then(() => console.log('🐰🐼🐼MongoDB Connected...✅✅✅ ... 🐼🐼🐰'))
-        .catch(err => console.log(err));
-}
